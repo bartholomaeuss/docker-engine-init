3a11723231cd68e3189e42ee1dc4bf93aad5231d
bart <bartholomaeus.s@googlemail.com>
2024-01-21 13:26:50 +0100

    Add script

--- install_docker_engine.sh
+++ install_docker_engine.sh
@@ -12,7 +12,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
 
 # Install latest version
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
